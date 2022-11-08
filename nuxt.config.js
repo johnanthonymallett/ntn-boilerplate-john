9d6ab536f66de6b8d27c49cc92c655352bf8b05f
johnanthonymallett <johnanthonymallett@gmail.com>
2022-11-08 14:27:47 +1300

    update icon title

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: SITE_INFO.sitename || process.env.npm_package_name || '',
+    title: 'John Anthony Mallett',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
